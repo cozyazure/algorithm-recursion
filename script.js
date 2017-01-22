e8c3b2d1ee331bdbc0c3abaf156ce770f8585a4f
Fang Hau <cozyazure@hotmail.com>
2017-01-23 01:02:19 +0800

    solve more problems

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 // Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
-
 // factorial(1) → 1
 // factorial(2) → 2
 // factorial(3) → 6
 var factorial = (x) => {
-        if (x == 1) {
-            return 1;
-        } else {
-            return x * factorial(x - 1);
-        }
-
+    if (x == 1) {
+        return 1;
+    } else {
+        return x * factorial(x - 1);
     }
-    // The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition. The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.
 
+}
+
+// The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition. The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.
 // fibonacci(0) → 0
 // fibonacci(1) → 1
 // fibonacci(2) → 1
@@ -111,7 +110,87 @@ var powerN = (base, power) => {
     }
 }
 
+
+// Given a string, compute recursively (no loops) the number of lowercase 'x' chars in the string.
+// countX("xxhixx") → 4
+// countX("xhixhix") → 3
+// countX("hi") → 0
+var countX = (input) => {
+    var isX = input[0] === 'x' ? 1 : 0;
+    return input.length === 1 ? isX : isX + countX(input.substring(1));
+}
+
+
+// Given a string, compute recursively (no loops) the number of times lowercase "hi" appears in the string.
+// countHi("xxhixx") → 1
+// countHi("xhixhix") → 2
+// countHi("hi") → 1
+var countHi = (input) => {
+    if (input.length <= 1) {
+        return 0;
+    }
+    var isHi = input[0] === 'h' && input[1] === 'i' ? true : false;
+    if (isHi) {
+        return 1 + countHi(input.substring(2));
+    }
+    return 0 + countHi(input.substring(1));
+}
+
+// Given a string, compute recursively (no loops) a new string where all the lowercase 'x' chars have been changed to 'y' chars.
+// changeXY("codex") → "codey"
+// changeXY("xxhixx") → "yyhiyy"
+// changeXY("xhixhix") → "yhiyhiy"
+var changeXY = (input) => {
+    var isX = input[0] === 'x' ? 'y' : input[0];
+    return input.length === 1 ? isX : isX + changeXY(input.substring(1));
+}
+
+// Given a string, compute recursively (no loops) a new string where all appearances of "pi" have been replaced by "3.14".
+// changePi("xpix") → "x3.14x"
+// changePi("pipi") → "3.143.14"
+// changePi("pip") → "3.14p"
+var changePi = (input) => {
+    if (input.length == 1) {
+        return input[0];
+    }
+    if (input.length <= 0) {
+        return '';
+    }
+    var isPi = input[0] === 'p' && input[1] === 'i' ? true : false;
+    if (isPi) {
+        return '3.14' + changePi(input.substring(2));
+    }
+    return input[0] + changePi(input.substring(1));
+}
+
+// Given a string, compute recursively a new string where all the 'x' chars have been removed.
+// noX("xaxb") → "ab"
+// noX("abc") → "abc"
+// noX("xx") → "
+var noX = (input) => {
+    var isX = input[0] === 'x' ? '' : input[0];
+    return input.length === 1 ? isX : isX + noX(input.substring(1));
+}
+
+// Given an array of ints, compute recursively if the array contains a 6. We'll use the convention of considering only the part of the array that begins at the given index. In this way, a recursive call can pass index+1 to move down the array. The initial call will pass in index as 0.
+// array6([1, 6, 4], 0) → true
+// array6([1, 4], 0) → false
+// array6([6], 0) → true
+
+var array6 = (arr, index) => {
+    if (index > 0) {
+        arr.slice(index);
+    }
+    var isSix = arr[0] === 6 ? true : false
+    if (arr.length === 1) {
+        return isSix;
+    } else {
+        arr.shift();
+        return isSix ? true : array6(arr, 0);
+    }
+}
+
 (() => {
     'use strict';
-    console.log(factorial(3));
+    console.log(array6([1,2,3,4,5], 0));
 })();
